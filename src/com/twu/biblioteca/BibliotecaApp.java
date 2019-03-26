1fbf5d58098c0981941c5c90eb3a81d581d9fd00
Jhonatan Silva <jhonatansilva96@gmail.com>
2019-03-26 11:54:48 -0300

    View a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,11 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    public static String welcomeMessage() {
+        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(welcomeMessage());
     }
 }
