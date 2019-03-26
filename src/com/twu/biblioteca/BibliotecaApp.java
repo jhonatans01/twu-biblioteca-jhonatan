7195652d7535815e69e2569397dcd515ebfdd95c
Jhonatan Silva <jhonatansilva96@gmail.com>
2019-03-26 16:47:28 -0300

    View a main menu of options

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,10 +3,48 @@ package com.twu.biblioteca;
 import com.twu.biblioteca.controller.BookController;
 import com.twu.biblioteca.model.Book;
 
+import java.util.*;
+
 public class BibliotecaApp {
 
-    public static String welcomeMessage() {
+    public static String getWelcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    }
+
+    public static List<String> getMenuOptions() {
+        List<String> options = new ArrayList<String>();
+        options.add("1 - List of books");
+        return options;
+    }
+
+    public static void printMenuOptions() {
+        List<String> menuOptions = BibliotecaApp.getMenuOptions();
+        for (String option: menuOptions) {
+            System.out.println(option);
+        }
+    }
+
+    public static Integer readOption() {
+        Scanner reader = new Scanner(System.in);
+        String option = reader.nextLine();
+        int intOption;
+        try {
+            intOption = Integer.parseInt(option);
+        } catch (NumberFormatException numberException) {
+            System.out.println("Only numbers are allowed");
+            return null;
+        }
+        return intOption;
+    }
+
+    public static boolean chooseOption(int option) {
+        switch (option) {
+            case 1:
+                printBooks();
+                return true;
+            default:
+                return false;
+        }
     }
 
     public static void printBooks() {
@@ -17,7 +55,12 @@ public class BibliotecaApp {
     }
 
     public static void main(String[] args) {
-        System.out.println(welcomeMessage());
-        printBooks();
+        System.out.println(getWelcomeMessage());
+        printMenuOptions();
+
+        Integer option = readOption();
+        if (option != null && !chooseOption(option)) {
+            System.out.println("This option is not available");
+        }
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,13 +1,50 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
 
+    private List<String> menuOptions;
+
+    @Before
+    public void setUp() {
+        menuOptions = BibliotecaApp.getMenuOptions();
+    }
+
     @Test
     public void shouldReturnWelcomeMessage() {
-        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", BibliotecaApp.welcomeMessage());
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!", BibliotecaApp.getWelcomeMessage());
+    }
+
+    @Test
+    public void shouldReturnOptionsMenu() {
+        assertNotNull(menuOptions);
+    }
+
+    @Test
+    public void shouldReturnOptionsMenuNotEmpty() {
+        assertTrue(!menuOptions.isEmpty());
+    }
+
+    @Test
+    public void shouldChooseMenuOptionWithErrorForNegativeNumber() {
+        assertFalse(BibliotecaApp.chooseOption(-1));
+
+    }
+
+    @Test
+    public void shouldChooseMenuOptionWithErrorForNonAvailableOption() {
+        assertFalse(BibliotecaApp.chooseOption(999));
+    }
+
+    @Test
+    public void shouldChooseMenuOptionWithSuccess() {
+        assertTrue(BibliotecaApp.chooseOption(1));
     }
 }
